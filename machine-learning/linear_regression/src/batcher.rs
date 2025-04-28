15dff309b45cbcfade1d48c0d4eaf24a4bd1b529
Thomas Wagener <thomas.wagener@fischantiquariat.de>
2025-04-28 14:45:34 +0200

    Changes batcher to adapt to model

--- machine-learning/linear_regression/src/batcher.rs
+++ machine-learning/linear_regression/src/batcher.rs
@@ -7,7 +7,7 @@ pub struct TaxifareBatcher;
 pub struct TaxifareBatch<B: Backend> {
     pub cont_features: Tensor<B, 3>,
     pub cat_features: Tensor<B, 3, Int>,
-    pub predictions: Tensor<B, 1>,
+    pub predictions: Tensor<B, 2>,
 }
 
 impl<B: Backend> Batcher<B, TaxifareDatasetMappedItem, TaxifareBatch<B>> for TaxifareBatcher {
@@ -32,11 +32,12 @@ impl<B: Backend> Batcher<B, TaxifareDatasetMappedItem, TaxifareBatch<B>> for Tax
         let predictions = items
             .iter()
             .map(|item| Tensor::<B, 1>::from_data([item.label], device))
+            .map(|tensor| tensor.reshape([1, 1]))
             .collect();
 
-        let cont_features = Tensor::cat(cont_features, 1);
-        let cat_features = Tensor::cat(cat_features, 1);
-        let predictions = Tensor::cat(predictions, 0);
+        let cont_features = Tensor::cat(cont_features, 2);
+        let cat_features = Tensor::cat(cat_features, 2);
+        let predictions = Tensor::cat(predictions, 1);
         TaxifareBatch {
             cont_features,
             cat_features,
